3ec1d3aa5d8c3668d57322884c11e0a96999e8c0
Nguyen-Dang-Khoa-04072004 <khoa47245@gmail.com>
2025-08-30 18:54:24 +0700

    feat: remove reload page when click to link

--- index.js
+++ index.js
@@ -1,29 +1,27 @@
-const navLinks = document.querySelectorAll(".menu .navbar ul li")
-const sections = document.querySelectorAll('section')
-const setActiveLink = link => {
-    navLinks.forEach(l => l.classList.remove("active"))
-        link.classList.add("active")
-        const sectionId = link.dataset.sectionId
-        const section = document.getElementById(sectionId)
-        console.log(section.offsetTop)
-        section.scrollIntoView({behavior: 'smooth'})
-}
+const navLinks = document.querySelectorAll(".menu .navbar ul li");
+const sections = document.querySelectorAll("section");
+const setActiveLink = (link) => {
+    navLinks.forEach((l) => l.classList.remove("active"));
+    link.classList.add("active");
+    const sectionId = link.dataset.sectionId;
+    const section = document.getElementById(sectionId);
+    section.scrollIntoView({ behavior: "smooth" });
+};
 
-navLinks.forEach(link => {
-    link.addEventListener('click', () => setActiveLink(link))
-})
+navLinks.forEach((link) => {
+  link.addEventListener("click", () => setActiveLink(link));
+});
 
-
-window.addEventListener('scroll', () => {
-    sections.forEach(section => {
-        if (section.offsetTop - window.scrollY < 20){
-            navLinks.forEach(link => {
-                if(link.dataset.sectionId !== section.id){
-                    link.classList.remove('active')
-                }else{
-                    link.classList.add('active')
-                }
-            })
+window.addEventListener("scroll", () => {
+  sections.forEach((section) => {
+    if (section.offsetTop - window.scrollY < 20) {
+      navLinks.forEach((link) => {
+        if (link.dataset.sectionId !== section.id) {
+          link.classList.remove("active");
+        } else {
+          link.classList.add("active");
         }
-    })
-})
+      });
+    }
+  });
+});
