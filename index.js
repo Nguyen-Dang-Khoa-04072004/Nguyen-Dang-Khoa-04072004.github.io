17e16f6a81afe08ce3e68e2378b0f17f3debc1d2
Nguyen-Dang-Khoa-04072004 <khoa47245@gmail.com>
2025-08-30 18:44:07 +0700

    feat: add click to scroll to a section  and scroll to active link

--- index.js
+++ index.js
@@ -1,7 +1,29 @@
 const navLinks = document.querySelectorAll(".menu .navbar ul li")
-navLinks.forEach(link => {
-    link.addEventListener('click', () => {
-        navLinks.forEach(l => l.classList.remove("active"))
+const sections = document.querySelectorAll('section')
+const setActiveLink = link => {
+    navLinks.forEach(l => l.classList.remove("active"))
         link.classList.add("active")
+        const sectionId = link.dataset.sectionId
+        const section = document.getElementById(sectionId)
+        console.log(section.offsetTop)
+        // section.scrollIntoView({behavior: 'smooth'})
+}
+
+navLinks.forEach(link => {
+    link.addEventListener('click', () => setActiveLink(link))
+})
+
+
+window.addEventListener('scroll', () => {
+    sections.forEach(section => {
+        if (section.offsetTop - window.scrollY < 20){
+            navLinks.forEach(link => {
+                if(link.dataset.sectionId !== section.id){
+                    link.classList.remove('active')
+                }else{
+                    link.classList.add('active')
+                }
+            })
+        }
     })
 })
